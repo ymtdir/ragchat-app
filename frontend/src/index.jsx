13cd9b4d4bfacafb8ec37144f5b9931cf9d44dee
y_matsudaira <y.matsudaira@digital-corp.co.jp>
2025-06-23 00:17:41 +0900

    feat: frontendの雛型を作成

---
+++ frontend/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
