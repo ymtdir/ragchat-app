6ead63345b1f7435109e56e752afd9de5ed898ec
ymtdir <ymtdir0407@gmail.com>
2025-07-21 20:10:35 +0900

    feat: signinフォームの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,11 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { ModeToggle } from "@/components/mode-toggle";
+import { SignInForm } from "@/components/signin-form";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <ModeToggle />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <SignInForm />
+    </div>
   );
 }
 
