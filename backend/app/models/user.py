474c957d6e1099dc5511ef15b7d07435f53b9ed3
ymtdir <ymtdir0407@gmail.com>
2025-09-17 19:44:17 +0900

    feat: ユーザー名およびメールアドレスのユニーク制約を削除

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -24,8 +24,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), unique=True, index=True, nullable=False)
-    email = Column(String(100), unique=True, index=True, nullable=False)
+    name = Column(String(50), index=True, nullable=False)
+    email = Column(String(100), index=True, nullable=False)
     password = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
